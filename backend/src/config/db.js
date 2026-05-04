15edd5c487b23f1ab3b8cbe56283c28ee64343f7
hasanhameed <hasanhameed858@gmail.com>
2026-05-04 20:26:37 +0530

    simplified node app2

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,25 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize("postgresql://postgres:Qwerty%409949719006@db.jbaveqntjbjllwaylzgw.supabase.co:6543/postgres", {
+// Always use the Environment Variable first
+const dbUrl = "postgresql://postgres:Qwerty%409949719006@db.jbaveqntjbjllwaylzgw.supabase.co:6543/postgres"; //"process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(dbUrl, {
     dialect: "postgres",
-    protocol: "postgres",
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false,
+            rejectUnauthorized: false, // Essential for Render -> Supabase
         },
     },
+    // Required when using PgBouncer/Transaction mode on Supabase
+    minifyAliases: true,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
 module.exports = sequelize;
