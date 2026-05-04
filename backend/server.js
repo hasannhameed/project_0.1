c9c787aae5a6ca27adf760937480ea8c52250014
hasanhameed <hasanhameed858@gmail.com>
2026-05-04 19:57:15 +0530

    simplified node app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// const sequelize = require('./src/config/db');
-// const User = require('./src/models/user.model');
+const sequelize = require('./src/config/db');
+const User = require('./src/models/user.model');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
