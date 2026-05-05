5b76625075997c4743753eb7e5c23f58da706255
hasanhameed <hasanhameed858@gmail.com>
2026-05-06 03:20:37 +0530

    simplified node app4

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,42 +1,8 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
-// Always use the Environment Variable first
-const dbUrl = "postgresql://postgres:Qwerty%409949719006@db.jbaveqntjbjllwaylzgw.supabase.co:6543/postgres"; //"process.env.DATABASE_URL;
-
-// const sequelize = new Sequelize(dbUrl, {
-//     dialect: "postgres",
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false, // Essential for Render -> Supabase
-//         },
-//     },
-//     // Required when using PgBouncer/Transaction mode on Supabase
-//     minifyAliases: true,
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// });
-
-
-
-
-const sequelize = new Sequelize(dbUrl, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    protocol: "postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
-    dialectModule: require("pg"),
-    // host: process.env.DB_HOST, // optional if splitting URL
+    logging: false,
 });
-
 
 module.exports = sequelize;
